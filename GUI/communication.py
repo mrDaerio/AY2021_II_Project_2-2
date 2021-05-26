Corrected sample_rate and used it for bpm calculation

GUI/communication.py
@@ -273,7 +273,7 @@ class KivySerial(EventDispatcher, metaclass=Singleton):
             if (diff != 0):
                 self.current_sample_rate = (self.samples_counter + 1) / diff
                 self.message_string = f'Samples: {self.samples_counter:6d} | Sample Rate: {self.current_sample_rate:5.2f} Hz'
-        self.samples_counter += 1
+        self.samples_counter += 32
 
     ##
     #   @brief          Serial data parser.
@@ -501,17 +501,24 @@ class Signal():
         self.filtered_sum = lfilter(b, a, [abs(i) for i in z_windowed])
 
         self.filtered_sum = np.abs(hilbert(self.filtered_sum))
-        if self.window_start_pos % 640 == 0:
-            self.peaks = find_peaks(self.filtered_sum[-640:], distance=80, prominence=0.12)
-            self.peaks = self.peaks[0]
-            self.diff = [t - s for s, t in zip(self.peaks, self.peaks[1:])]
-            self.diff = [i/200 for i in self.diff]
-            self.meanbpm = 60/np.mean(self.diff)
-            print(self.meanbpm)
+        #if self.window_start_pos % 640 == 0:
+        self.peaks = find_peaks(self.filtered_sum[-640:], distance=80, prominence=0.02)
+        self.peaks = self.peaks[0]
+        self.diff = [t - s for s, t in zip(self.peaks, self.peaks[1:])]
+        
+        board = KivySerial()
+        self.diff = [i/board.current_sample_rate if board.current_sample_rate != 0 else 0 for i in self.diff]
+
+
+        self.meanbpm = 60/np.mean(self.diff)
+        print(self.meanbpm)
         #if(self.flag_first_filter):
         self.filtered_sum = self.filtered_sum[1000-16:1000+16]
 
         self.window_start_pos += self.stride
 
     def get_filtered_data(self):
-        return self.filtered_sum[-32:]
+        logic_peak = [1 if i in self.peaks else 0 for i in range(640)]
+        print(self.peaks)
+        return logic_peak
+        #return self.filtered_sum[-32:]